16e9b6731054ad057403b8331dafd168c0b84f52
Thien Trieu <thien.trieu@telus.com>
2022-11-14 17:50:36 -0800

    Adding head function - to find first element of an array

---
+++ head.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(5, 7);
+
+
+// Create a function head which
+// returns the first item in the array.
+// The head function should not return the first element as an array.
+// It should simply return the element itself.
+
+const head = function(array) {
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array.length > 0) {
+      return array[0];
+    }
+  }
+  if (array.length < 0) {
+    return undefined;
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
