98e241f2ca4b10bb0bef88180d5b906b04ec55ac
Thien Trieu <thien.trieu@telus.com>
2022-11-14 21:40:46 -0800

    Adding reverse function for reversing spelling of string

---
+++ reverse.js
@@ -0,0 +1,18 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+let reverse = function(input) {
+  let newString = "";
+  for (let i = 0; i < input.length; i++) {
+    for (let x = input[i].length - 1; x >= 0; x--) {
+      if (input[i][x] !== undefined)
+        newString += input[i][x];
+    }
+    if (i !== input.length - 1) {
+      newString += "\n";
+    }
+  }
+  console.log(newString);
+};
+
+reverse(args);
