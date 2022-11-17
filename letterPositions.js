396b346449f8a432b89cdde203721e64219a28ad
Thien Trieu <thien.trieu@telus.com>
2022-11-16 19:29:37 -0800

    Adding letterPositions function - Object containing index numbers of each letter in a string

---
+++ letterPositions.js
@@ -0,0 +1,59 @@
+// function to check if values of two aray are equal
+const eqArrays = function(arr1, arr2) {
+  let len = arr1.length;
+  for (let i = 0; i < len; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Logging if Assertion of Arrays
+function assertArraysEqual(arr1, arr2) {
+  // Storing return of eqArray in result
+  let result = eqArrays(arr1, arr2);
+  if (result === true) {
+    console.log(`🤑 Assertion Passed: ${arr1} === ${arr2}`);
+    return;
+  }
+  if (result === false) {
+    console.log(`😡 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+
+const letterPositions = function(sentence) {
+  const stringArr = sentence.split("");
+  const results = {};
+
+  for (const index in stringArr) {
+    if (stringArr[index] === " ") {
+      continue;
+    }
+
+    if (results[stringArr[index]]) {
+      results[stringArr[index]].push(index);
+
+    } else {
+      results[stringArr[index]] = [index];
+    }
+  }
+
+  return results;
+};
+console.log(letterPositions("lighthouse in the house"))
+/*
+{ 
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+}
+*/
