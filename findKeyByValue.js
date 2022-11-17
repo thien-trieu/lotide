d4ba466a973f64a455faebb0d2efa3f681ac726d
Thien Trieu <thien.trieu@telus.com>
2022-11-16 19:39:34 -0800

    adding findKeyByValue function - search for a value in an object and return the key

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value) {
+  result = undefined;
+  for (const key in object) {
+    if (object[key] === value) {
+      result = `${key}`;
+    }
+  }
+  return result;
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
