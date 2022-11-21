088ff1244cfd9d49b856d20dfbdc2206aaf4e8b6
Thien Trieu <thien.trieu@telus.com>
2022-11-20 22:21:19 -0800

    refactored assertEqual, module exports added and separated test codes.

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-
-assertEqual("Lighthouse Labs", "Lighthouse Labs"); // should pass
-assertEqual("Lighthouse Labs", "Lighthouse"); // should fail
-assertEqual(7, 7); // should pass
-assertEqual(5, 7); // should fail
+module.exports = assertEqual;
